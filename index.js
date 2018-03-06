02fac29e2f515966a7d94d2b7f12714e3e5aea2e
Pete A <peter.ayresmusic@gmail.com>
2018-03-06 20:41:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ function destructivelyUpdateObjectWithKeyAndValue(object,key,value){
     object[key] = value
     return object
 }
-function deleteFromObjectByKey(obj,key){
+function deleteFromObjectByKey(object,key){
   delete newObj['prop']
 }
