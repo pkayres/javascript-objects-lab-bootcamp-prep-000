619383c4974ee440f10a56ce5bc8d6a418a909d6
Pete A <peter.ayresmusic@gmail.com>
2018-03-06 20:04:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var recipes = {
 }; 
 
 function updateObjectWithKeyAndValue(object, key, value){ 
-  return Object.assign({},obj,{ [key]: value })
+  return Object.assign({}, recipes,{ [key]: value })
   }
   
   
