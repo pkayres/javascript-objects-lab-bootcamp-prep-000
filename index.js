acd3fa861f369cfdb9a910a41f88b0f75880fe4d
Pete A <peter.ayresmusic@gmail.com>
2018-03-06 20:11:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,4 +6,6 @@ function updateObjectWithKeyAndValue(obj, key, value){
   return Object.assign({}, obj,{ [key]: value })
   }
 
-function destructivelyUpdateObjectWithKeyAndValue(object,key,value)
+function destructivelyUpdateObjectWithKeyAndValue(object,key,value){
+  
+}
